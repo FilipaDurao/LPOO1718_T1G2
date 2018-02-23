eaf9556110a99d2362d7ff37387deadb18aa1298
xRuiAlves <up201606746@fe.up.pt>
2018-02-23 21:47:26 +0000

    Implemented 'Club' class

---
+++ DungeonKeep/Club.java
@@ -0,0 +1,15 @@
+
+public class Club extends GameObject {
+
+	private final char idSymbol = '*';
+
+	public Club(int x_pos, int y_pos) {
+		super(x_pos, y_pos);
+	}
+
+	@Override
+	public char getIdSymbol() {
+		return idSymbol;
+	}
+
+}
