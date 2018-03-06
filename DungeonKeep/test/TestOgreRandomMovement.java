73449ca5fc5347a0ff41449043784c2b87fe5bf1
filipa <filipadurao@gmail.com>
2018-03-06 18:36:47 +0000

    Started Iteration 3 Task 3

---
+++ DungeonKeep/test/TestOgreRandomMovement.java
@@ -0,0 +1,24 @@
+package DungeonKeep.test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.ArrayList;
+
+import DungeonKeep.logic.*;
+
+public class TestOgreRandomMovement {
+
+	@Test(timeout = 1000)
+	public void testOgreRandomMovement() {
+		
+		while(true) {
+			
+		}
+		
+		
+	}
+	
+	
+}
