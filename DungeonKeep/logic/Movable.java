dd352930009329cde18903a3853b236986cb1a66
FilipaDurao <up201606746@fe.up.pt>
2018-04-01 17:00:36 +0100

    Implemented MovableGameObject abstract class

--- DungeonKeep/logic/Movable.java
+++
@@ -1,7 +0,0 @@
-package DungeonKeep.logic;
-
-import DungeonKeep.logic.GameObject.MoveDirection;
-
-public interface Movable {
-	public void move(MoveDirection dir);
-}
---
+++ DungeonKeep/logic/MovableGameObject.java
@@ -0,0 +1,44 @@
+package DungeonKeep.logic;
+
+public abstract class MovableGameObject extends GameObject {
+	
+	public enum MoveDirection {UP, DOWN, RIGHT, LEFT, INVALID}
+	
+	public MovableGameObject(int x_pos, int y_pos) {
+		super(x_pos, y_pos);
+	}
+
+	public void move (MoveDirection dir) {
+		switch (dir) {
+		case UP:
+			this.setY_pos(this.getY_pos() - 1);
+			break;
+		case DOWN:
+			this.setY_pos(this.getY_pos() + 1);
+			break;
+		case RIGHT:
+			this.setX_pos(this.getX_pos() + 1);
+			break;
+		case LEFT:
+			this.setX_pos(this.getX_pos() - 1);
+			break;
+		default:
+			return;
+		}		
+	}
+	
+	public static MoveDirection getOppositeMoveDirection(MoveDirection dir) {
+		if (dir == MoveDirection.UP) {
+			return MoveDirection.DOWN;
+		}
+		else if (dir == MoveDirection.DOWN) {
+			return MoveDirection.UP;
+		}
+		else if (dir == MoveDirection.RIGHT) {
+			return MoveDirection.LEFT;
+		}
+		else {
+			return MoveDirection.RIGHT;
+		}
+	}
+}
